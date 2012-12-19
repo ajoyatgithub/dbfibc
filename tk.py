2be9caf4cc27a9f5455203707277940bfe7a162e
Ajoy <ajoy@ajoy.(none)>
2012-12-19 21:40:43 +0530

    pygtk

--- tk.py
+++ tk.py
@@ -1,11 +1,25 @@
-# File: hello1.ptexy
+# File: hello2.py
 
 from Tkinter import *
 
+class App:
+
+    def __init__(self, master):
+
+        frame = Frame(master)
+        frame.pack()
+
+        self.button = Button(frame, text="QUIT", fg="red", command=frame.quit)
+        self.button.pack(side=LEFT)
+
+        self.hi_there = Button(frame, text="Hello", command=self.say_hi)
+        self.hi_there.pack(side=LEFT)
+
+    def say_hi(self):
+        print "hi there, everyone!"
+
 root = Tk()
 
-w = Label(root, text="Hello, world!\n hoe are u")
-s = Label(root, text="This is not empty         \n                     ")
-w.pack()
-s.pack()
+app = App(root)
+
 root.mainloop()
