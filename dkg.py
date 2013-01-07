4b458e9a27ed88a20fedb3bcdc554ba40ff0e58a
Ajoy <ajoy91ad@gmail.com>
2013-01-08 00:44:45 +0530

    Added dkg code

--- dkg.py
+++ dkg.py
@@ -1,4 +1,5 @@
 import random
+import threading
 from ctypes import cdll
 
 secretshare = 683425
@@ -10,8 +11,19 @@ def dkg():
   
   #main is the main function in node.cc. To prevent errors, the library was created without BLSclient.o
   
-  #libnode.main(7, "./node 6401 ../src/certs/1.pem ../src/certs/1-key.pem contlist 0 0 0")
+  libnode.main()
+  #libnode.main(5, "./node 6401 ../src/certs/1.pem ../src/certs/1-key.pem contlist")
   secretshare = random.random()
   
 def share():
+  secretshare = random.random()
   return secretshare
+  
+
+class MyThread ( threading.Thread ):
+   def run ( self ):
+      print 'Insert some thread stuff here.'
+      print 'It'll be executed...yeah....'
+      print 'There's not much to it.'
+
+MyThread().start()
