c7d1c080ab753dec56dce24fdff799727ff5d016
Ajoy <ajoy91ad@gmail.com>
2013-01-12 12:59:29 +0530

    prints share and commitment - as soon as the dkg completes, the share and commitment values are printed to the secrets file

--- dkg/node.cc
+++ dkg/node.cc
@@ -1223,6 +1223,7 @@ void Node::completeDKG(){
 	FILE *fp;
 	fp = fopen("../secrets","w");
 	result.share.dump(fp,(char*)"Share is ",10);
+	result.C.dump(fp);
 	fclose(fp);
 	//DecidedVSSs broadcast and decided VSSs are now completed
 	nodeState = DKG_COMPLETED;
