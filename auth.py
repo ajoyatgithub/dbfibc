b2792852df4f8118ef6833dd33531c4d79d1a2fd
Ajoy <ajoy91ad@gmail.com>
2013-02-18 07:06:08 +0530

    in auth.py masked the password

--- auth.py
+++ auth.py
@@ -7,22 +7,22 @@ def auth(username, password, auth_type):
     parse = re.search("([\w\.]*[\w])[@]([\w.]*[\w])", username)
     if parse == None:
       result = "Invalid email address"
-      #return result
+      return result
       return "S"
     uname = parse.group(1)
     domain_name = parse.group(2)
     if domain_name == "gmail.com":
-      #mailserver = smtplib.SMTP("smtp.gmail.com", 587)
-      #mailserver.ehlo()
-      #mailserver.starttls()
-      #mailserver.ehlo()
-      #(code, msgstr) = mailserver.login(username,password)
+      mailserver = smtplib.SMTP("smtp.gmail.com", 587)
+      mailserver.ehlo()
+      mailserver.starttls()
+      mailserver.ehlo()
+      (code, msgstr) = mailserver.login(username,password)
       code = 235
       if code == 235:
         result = "S"
       else:
         result = msgstr
-      #mailserver.close()
+      mailserver.close()
     else:
       print("Sorry, " + domain_name + " is not supported. Currently only GMail authentication is available\n")
       result = "Unsupported"
--- dbfibc.py
+++ dbfibc.py
@@ -7,6 +7,7 @@ import dkg
 import ibc
 import app
 import sys
+import getpass
 
 def main():
   print "**************WELCOME****************\n"
@@ -20,7 +21,7 @@ def main():
     auth_str = "LDAP username"
   print "Please enter the " + auth_str + " that you want to use as your identity : "
   username = raw_input(auth_str + " : ")
-  password = raw_input("Please enter the password for your " + auth_str + " '" + username + "' : ")
+  password = getpass.getpass("Please enter the password for your " + auth_str + " '" + username + "' : ")
   hashp = md5.new(password).digest()
   auth_result = auth.auth(username, password, auth_type)
   if  auth_result is "S":
