2fcdac833be1e70cfdea7f415d44d7fd08e734bc
Ajoy Oommen <ajoy91ad@gmail.com>
2015-02-20 00:05:35 +0530

    Modify extra lines in sent.py

--- sent.py
+++ sent.py
@@ -8,9 +8,6 @@ def say():
     label=Label(frame,text="To",fg="blue",relief=RAISED,width=50,bg="white")
     label.pack(padx=10,pady=10)
     listbox = Listbox(frame,relief=RAISED,width=50)
-    
-    
-    
     name=open("../contacts","r")
     while 1:
         line = name.readline()
@@ -19,13 +16,13 @@ def say():
         line =line.rstrip("\n")
         pr = re.search("(ID:[\d]+:name:)([\S]+)",line)
         listbox.insert(END, pr.group(2))
-    
+
     listbox.pack(padx=10,pady=10)
     text = Text(frame, height=10, width=57)
     text.pack(padx=10,pady=10)
-    
+
     send=Button(frame,text="SEND",bg="green",relief=RAISED,width=47)
     send.pack(padx=10,pady=10)
-    
+
     Quit=Button(frame,text="QUIT",bg="green",command=top.destroy,relief=RAISED,width=47)
-    Quit.pack(padx=10,pady=10)    
+    Quit.pack(padx=10,pady=10)
