76da373b0ffff7f32af032c95f8c58400f5763bf
Ajoy Oommen <ajoy91ad@gmail.com>
2015-02-20 00:04:01 +0530

    Add spaces to dkg.py

--- dkg.py
+++ dkg.py
@@ -10,40 +10,42 @@ tempcontlist = []
 status = "nosecret"
 
 def onChange(ev):
-  global status
-  status = "sharegen"
+    global status
+    status = "sharegen"
 
 def watch_secrets():
-  wm = pyinotify.WatchManager()
-  wm.add_watch('files/secrets', pyinotify.IN_CLOSE_WRITE, onChange)
-  notifier = pyinotify.Notifier(wm)
-  notifier.loop()
-  
+    wm = pyinotify.WatchManager()
+    wm.add_watch('files/secrets', pyinotify.IN_CLOSE_WRITE, onChange)
+    notifier = pyinotify.Notifier(wm)
+    notifier.loop()
+
 def dkg(nid):
-  global nodeid
-  #print "DKG starting...."
-  threading.Thread(target=startdkg).start()
-  threading.Thread(target=watch_secrets).start()
-  #print "DKG started...."
-  #startdkg()
-  
+    global nodeid
+    #print "DKG starting...."
+    threading.Thread(target=startdkg).start()
+    threading.Thread(target=watch_secrets).start()
+    #print "DKG started...."
+    #startdkg()
+
 def startdkg():
-  global nodeid
-  fp = open("files/contlist","r")
-  while 1:
-    line = fp.readline()
-    if not line:
-      break
-    parse = re.search("([\S]+)\s([\S]+)\s([\S]+)\s([\S]+)\s([L]*)", line)
-    tempcontlist.append([parse.group(1), parse.group(2), parse.group(3), parse.group(4), parse.group(5)])
-  fp.close()
-  [nid, c_ip, c_port, cert_file, l] = tempcontlist[int(nodeid)-1]
-  ltr = "./dkg/node %d certs/%d.pem certs/%d-key.pem files/contlist 0 0 0" % (int(c_port), int(nid), int(nid))
-  print nodeid, ltr
-  #fp = open("dkg/rundkg","w")
-  #fp.write(ltr)
-  #fp.close()
-  #ltr = "../dkg/node %d ../dkg/certs/%d.pem ../dkg/certs/%d-key.pem ../dkg/contlist 0 0 0" % (int(c_port), int(nid), int(nid))
-  #print ltr
-  #os.chdir("dkg/")
-  subprocess.call(ltr)
+    global nodeid
+    fp = open("files/contlist","r")
+    while 1:
+        line = fp.readline()
+        if not line:
+            break
+        parse = re.search("([\S]+)\s([\S]+)\s([\S]+)\s([\S]+)\s([L]*)", line)
+        tempcontlist.append([parse.group(1), parse.group(2), parse.group(3),
+                             parse.group(4), parse.group(5)])
+    fp.close()
+    [nid, c_ip, c_port, cert_file, l] = tempcontlist[int(nodeid)-1]
+    ltr = ("./dkg/node %d certs/%d.pem certs/%d-key.pem files/contlist 0 0 0"
+           % (int(c_port), int(nid), int(nid)))
+    print nodeid, ltr
+    #fp = open("dkg/rundkg","w")
+    #fp.write(ltr)
+    #fp.close()
+    #ltr = "../dkg/node %d ../dkg/certs/%d.pem ../dkg/certs/%d-key.pem ../dkg/contlist 0 0 0" % (int(c_port), int(nid), int(nid))
+    #print ltr
+    #os.chdir("dkg/")
+    subprocess.call(ltr)
